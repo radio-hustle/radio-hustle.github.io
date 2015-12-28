22baa24d1383727bc675aa08f868a85a7fce6db2
tribunskiy <forty-7@yandex.ru>
2015-12-28 12:59:43 +0300

    update

---
+++ projects/live/js/lang.js
@@ -0,0 +1,59 @@
+﻿var Lang = {
+    defaultLang: 'en',
+    langs: ['en', 'ru'],
+    tokens: {
+        '1': {
+            'en': 'Just missed',
+            'ru': 'Только что было'
+        },
+        '2': {
+            'en': 'Change language',
+            'ru': 'Сменить язык'
+        }
+    },
+    init: function (callBack) {
+        if ('localStorage' in window && window['localStorage'] !== null) {
+            localStorage.setItem('lang-default', Lang.defaultLang);
+            var lang = localStorage.getItem('lang');
+            if (lang === undefined || lang === null) {
+                lang = Lang.defaultLang;
+                localStorage.setItem('lang', lang);
+            }
+            for (var i in Lang.tokens) {
+                var element = document.querySelector('[data-lang-id="' + i + '"]');
+                //element.setAttribute('data-lang-current', lang);
+                element.innerText = Lang.tokens[i][lang];
+            }
+            callBack = callBack || function () {};
+            callBack();
+        }
+    },
+    changeLanguage: function (lang) {
+        if (lang === undefined || lang === null) {
+            var langs = Lang.langs,
+                currentLang = localStorage.getItem('lang'),
+                currentLangIndex,
+                nextLang,
+                nextLangIndex;
+            if (currentLang == undefined) {
+                currentLang = Lang.defaultLang;
+            }
+            currentLangIndex = langs.indexOf(currentLang);
+            if (currentLangIndex > langs.length - 2) {
+                nextLangIndex = 0;
+            } else {
+                nextLangIndex = currentLangIndex + 1;
+            }
+            nextLang = langs[nextLangIndex];
+            console.log(nextLang);
+            lang = nextLang;
+        }
+        for (var i in Lang.tokens) {
+            var element = document.querySelector('[data-lang-id="' + i + '"]');
+            element.innerText = Lang.tokens[i][lang];
+        }
+        localStorage.setItem('lang', lang);
+    }
+};
+
+Lang.init();
--- projects/live/js/main.js
+++ projects/live/js/main.js
@@ -2,7 +2,52 @@ $(function () {
     $('.equalizer').rhEqualizer();
     $('.equalizer').rhEqualizer('setContainerPadding', ($(window).height() / 2 - 70) + 'px 0 0 0');
 
+    $('.btn-change-language').on('click', function () {
+        Lang.changeLanguage();
+    });
+
+    $.SHOUTcast({
+        host: '93.188.164.219',
+        port: 8000,
+        interval: 2000,
+        stats: function () {
+            var songTitle = this.get('songtitle'),
+                artist = songTitle.split(' - ')[0].trim().replace(/\d{1,3}\sbmp/, ''),
+                title = songTitle.split(' - ')[1].trim().replace(/\d{1,3}\sbpm/, '');
+            $('.shoutcast-now-playing.current-artist').text(artist);
+            $('.shoutcast-now-playing.current-track').text(title);
+        }
+    }).startStats();
+
+    $.SHOUTcast({
+        host: '93.188.164.219',
+        port: 8000,
+        playedInterval: 20000,
+        played: function (tracks) {
+            $('#played').html('');
+            $.each(tracks, function (k, track) {
+                if (k > 0 && k < 6) {
+                    $('#played').append('<div class="cctrack"><div class="ccinfos"><div class="cctitle">' + track.title.replace(/\d{1,3}\sbpm/, '') + '</div></div></div>');
+                }
+            });
+        }
+    }).startPlayed();
+
     $(window).resize(function () {
         $('.equalizer').rhEqualizer('setContainerPadding', ($(window).height() / 2 - 70) + 'px 0 0 0');
+    });
+
+    var audio = document.getElementById('player');
+
+    audio.addEventListener('play', function () {
+        $('.equalizer').rhEqualizer('toggleActive', 'start');
+    });
+
+    audio.addEventListener('pause', function () {
+        $('.equalizer').rhEqualizer('toggleActive', 'stop');
+    });
+
+    audio.addEventListener('ended', function () {
+        $('.equalizer').rhEqualizer('toggleActive', 'stop');
     })
 });
