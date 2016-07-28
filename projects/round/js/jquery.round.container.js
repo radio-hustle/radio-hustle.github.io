56409891d3f3468611d76f025b9877119815c990
tribunskiy <forty-7@yandex.ru>
2016-07-28 09:36:06 +0300

    update

--- projects/round/js/jquery.round.container.js
+++ projects/round/js/jquery.round.container.js
@@ -1,7 +1,1 @@
-!function (o) {
-    "use strict";
-    o.zoomooz || (o.zoomooz = {}), o.fn.zoomContainer = function (o) {
-    }, o(document).ready(function () {
-        o(".zoomContainer").zoomContainer()
-    })
-}(jQuery);
+!function(o){"use strict";o.zoomooz||(o.zoomooz={}),o.fn.zoomContainer=function(o){},o(document).ready(function(){o(".zoomContainer").zoomContainer()})}(jQuery);
--- projects/round/js/jquery.round.helpers.js
+++ projects/round/js/jquery.round.helpers.js
@@ -1,13 +1,1 @@
-$.zoomooz || ($.zoomooz = {}), $.zoomooz.helpers = function (o, r) {
-    "use strict";
-    var n = ["-moz-", "-webkit-", "-o-", "-ms-"];
-    return r.forEachPrefix = function (o, r) {
-        for (var e = 0; e < n.length; e++)o(n[e]);
-        r && o("")
-    }, r.getElementTransform = function (n) {
-        var e;
-        return r.forEachPrefix(function (r) {
-            e = e || o(n).css(r + "transform")
-        }, !0), e
-    }, r
-}(jQuery, {});
+$.zoomooz||($.zoomooz={}),$.zoomooz.helpers=function(o,r){"use strict";var n=["-moz-","-webkit-","-o-","-ms-"];return r.forEachPrefix=function(o,r){for(var e=0;e<n.length;e++)o(n[e]);r&&o("")},r.getElementTransform=function(n){var e;return r.forEachPrefix(function(r){e=e||o(n).css(r+"transform")},!0),e},r}(jQuery,{});
